48dcf12d74d4c8259da4785ee7bf91bb9274b937
AnjaliGaneshram <97338343+AnjaliGaneshram@users.noreply.github.com>
2022-01-15 18:38:30 +0530

    Add files via upload

---
+++ Data Conversion.py
@@ -0,0 +1,135 @@
+print("Data Conversion")
+print("From Integer to Float")
+#Int - float
+value1 = 1
+print(value1)
+print(type(value1))
+value2 = float(value1)
+print(value2)
+print(type(value2))
+print("\nFrom Float to Integer")
+
+#float - Int
+value1 = 2.5
+print(value1)
+print(type(value1))
+value2 = int(value1)
+print(value2)
+print(type(value2))
+
+print("\nFrom Integer to Boolean")
+#int - bool
+
+value1 = 9
+print(value1)
+print(type(value1))
+value2 = bool(value1)
+print(value2)
+print(type(value2))
+
+print("\nFrom Boolean to Integer")
+#bool - int
+value1 = True
+print(value1)
+print(type(value1))
+value2 = int(value1)
+print(value2)
+print(type(value2))
+
+print("\nFrom Boolean to Float")
+#bool - float
+
+value1 = False
+print(value1)
+print(type(value1))
+value2 = float(value1)
+print(value2)
+print(type(value2))
+
+print("\nFrom Float to Boolean")
+#float - bool
+
+value1 = -0.01
+print(value1)
+print(type(value1))
+value2 = bool(value1)
+print(value2)
+print(type(value2))
+
+print("\nFrom Integer to String")
+#int - string
+
+value1 = 10
+print(value1)
+print(type(value1))
+value2 = str(value1)
+print(value2)
+print(type(value2))
+
+print("\nFrom String to Integer")
+#string - int
+
+value1 = "12"
+print(value1)
+print(type(value1))
+value2 = int(value1)
+print(value2)
+print(type(value2))
+
+'''
+#str - int
+
+value1 = "apple"
+print(value1)
+print(type(value1))
+value2 = int(value1)
+print(value2)
+print(type(value2))
+'''
+print("\nFrom Float to String")
+#float - string
+
+
+value1 = 10.5
+print(value1)
+print(type(value1))
+value2 = str(value1)
+print(value2)
+print(type(value2))
+
+print("\nFrom String to Float")
+#str - float
+value1 = "10.12"
+print(value1)
+print(type(value1))
+value2 = float(value1)
+print(value2)
+print(type(value2))
+
+print("\nFrom Boolean to String")
+
+#bool - str
+
+value1 = True
+print(value1)
+print(type(value1))
+value2 = str(value1)
+print(value2)
+print(type(value2))
+
+print("\nFrom String to Boolean")
+#str - bool
+
+value1 = 'apple'
+print(value1)
+print(type(value1))
+value2 = bool(value1)
+print(value2)
+print(type(value2))
+
+
+
+
+
+
+
