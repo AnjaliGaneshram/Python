b57b8695a743b7a9a4a397aea08e8c603abf297a
AnjaliGaneshram <97338343+AnjaliGaneshram@users.noreply.github.com>
2022-01-16 17:04:09 +0530

    Add files via upload

---
+++ Area and Perimeter of a Circle.py
@@ -0,0 +1,9 @@
+print("Area & Perimeter of the Circle")
+
+rad = float(input("Enter the radius value:"))
+pi = 3.14
+area = int(pi * (rad ** 2))
+perimeter = int(2 * pi * rad)
+print("Area of Circle = ",area)
+print("Perimeter of the Circle = ",perimeter)
+
